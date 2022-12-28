47f6b7f80334801d8f93866dbc2b8f4245764977
Tetiana Zykova <tatiana.nv96@gmail.com>
2022-12-28 04:08:47 +0200

    change on hooks

--- src/components/FormContact/FormContact.js
+++ src/components/FormContact/FormContact.js
@@ -1,85 +1,83 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useState, useRef } from 'react';
 import { nanoid } from 'nanoid';
 import { Box } from './../../common/Box';
 import { Input, LabelInput, ButtonSubmit } from './FormContact.styled';
 import { theme } from 'common/theme';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
+const FormContact = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const { current: nameInputId } = useRef(nanoid());
+  const { current: numberInputId } = useRef(nanoid());
+
+  const handleChangeName = e => setName(e.target.value);
+  const handleChangeNumber = e => setNumber(e.target.value);
+
+  const handleSubmitFormContact = e => {
+    e.preventDefault();
+
+    const trimName = name.trim();
+    const trimNumber = number.trim();
+
+    if (!trimName || !trimNumber) {
+      return;
+    }
+
+    onSubmit({ name: trimName, number: trimNumber });
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <Box
+      as="form"
+      p="10px"
+      border={theme.borders.normal}
+      borderColor={theme.colors.border}
+      mb={theme.space[3]}
+      onSubmit={handleSubmitFormContact}
+    >
+      <LabelInput htmlFor={nameInputId}>Name</LabelInput>
+      <Input
+        id={nameInputId}
+        type="text"
+        name="name"
+        value={name}
+        placeholder="Rosie Simpson"
+        onChange={handleChangeName}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+
+      <LabelInput htmlFor={numberInputId}>Number</LabelInput>
+      <Input
+        id={numberInputId}
+        type="tel"
+        name="number"
+        value={number}
+        placeholder="459-12-56"
+        onChange={handleChangeNumber}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+
+      <ButtonSubmit type="submit" disabled={!name || !number}>
+        Add
+      </ButtonSubmit>
+    </Box>
+  );
 };
 
-export class FormContact extends Component {
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  state = { ...INITIAL_STATE };
-
-  handleChangeState = e => {
-    const { name, value } = e.target;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmitFormContact = e => {
-    e.preventDefault();
-    this.props.onSubmit({ ...this.state });
-    this.resetForm();
-  };
-
-  resetForm = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
-
-  nameInputId = nanoid();
-  numberInputId = nanoid();
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <Box
-        as="form"
-        p="10px"
-        border={theme.borders.normal}
-        borderColor={theme.colors.border}
-        mb={theme.space[3]}
-        onSubmit={this.handleSubmitFormContact}
-      >
-        <LabelInput htmlFor={this.nameInputId}>Name</LabelInput>
-        <Input
-          id={this.nameInputId}
-          type="text"
-          name="name"
-          value={name}
-          placeholder="Rosie Simpson"
-          onChange={this.handleChangeState}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <LabelInput htmlFor={this.numberInputId}>Number</LabelInput>
-        <Input
-          id={this.numberInputId}
-          type="tel"
-          name="number"
-          value={number}
-          placeholder="459-12-56"
-          onChange={this.handleChangeState}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-
-        <ButtonSubmit type="submit" disabled={!name || !number}>
-          Add
-        </ButtonSubmit>
-      </Box>
-    );
-  }
-}
+FormContact.prototype = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default FormContact;
